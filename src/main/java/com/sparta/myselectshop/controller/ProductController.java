1923e44d306dc0c7f53dad89890e3c5d88ec8fd0
jeongho <jeongholee0918@gmail.com>
2024-04-25 16:05:14 +0900

    필터에 대한 이해

---
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.sparta.myselectshop.controller;
+
+
+
+import com.sparta.myselectshop.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+// 필터 검증을 위한 컨트롤러
+@Controller
+@RequestMapping("/api")
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts(HttpServletRequest req) {
+        System.out.println("ProductController.getProducts : 인증 완료");
+        User user = (User) req.getAttribute("user");
+        System.out.println("user.getUsername() = " + user.getUsername());
+
+        return "redirect:/";
+    }
+}
