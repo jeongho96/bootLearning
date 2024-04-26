79c331cd6588de521f1bdf29faf2dd18b16c8d4b
jeongho <jeongholee0918@gmail.com>
2024-04-26 15:19:45 +0900

    영속성 전이

--- jpa_advanced/src/main/java/com/sparta/jpaadvanced/entity/User.java
+++ jpa_advanced/src/main/java/com/sparta/jpaadvanced/entity/User.java
@@ -20,6 +20,11 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}) // 저장, 삭제시 영속성에 영향
     private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this); // 외래 키 설정
+    }
 }
