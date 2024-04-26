651f80c999b8022b520fcad9fc13a24229069d79
jeongho <jeongholee0918@gmail.com>
2024-04-26 14:54:32 +0900

    주문일 추가(Order)

--- jpa_advanced/src/main/java/com/sparta/jpaadvanced/JpaAdvancedApplication.java
+++ jpa_advanced/src/main/java/com/sparta/jpaadvanced/JpaAdvancedApplication.java
@@ -2,8 +2,11 @@ package com.sparta.jpaadvanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JpaAdvancedApplication {
 
     public static void main(String[] args) {
