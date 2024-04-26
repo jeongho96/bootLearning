542ec6ba438b4cc267996f0be9931f3ade723bde
jeongho <jeongholee0918@gmail.com>
2024-04-26 16:22:31 +0900

    myselectshop init commit

---
+++ MySelectShop_main/src/main/java/com/sparta/myselectshop/MySelectShopMainApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySelectShopMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySelectShopMainApplication.class, args);
+    }
+
+}
