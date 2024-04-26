e9c9a188fbe630a1c7cb0edefed4f3caa08fa441
jeongho <jeongholee0918@gmail.com>
2024-04-26 13:55:39 +0900

    JPA init commit

---
+++ jpa_advanced/src/test/java/com/sparta/jpaadvanced/JpaAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.jpaadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
