e2b30a4fa331467b52461f40a3e3fa07495ea3f1
jeongho <jeongholee0918@gmail.com>
2024-04-26 13:30:38 +0900

    restTemplate Get 요청

--- spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
+++ spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
@@ -1,21 +1,68 @@
 package com.sparta.springresttemplateclient.service;
 
 import com.sparta.springresttemplateclient.dto.ItemDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
+
 public class RestTemplateService {
 
+    private final RestTemplate restTemplate;
+
+    // restTemplate은 builder를 호출해서 주입받아야 함.
+    public RestTemplateService(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
     public ItemDto getCallObject(String query) {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/get-call-obj")
+                .queryParam("query", query)
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        // GET방식으로 서버에 요청을 보낼 때, uri로 요청을 보내고, ItemDto형태로 값을 받아옴.
+        // 알아서 직렬화 역직렬화 처리는 해줌.
+        ResponseEntity<ItemDto> responseEntity = restTemplate.getForEntity(uri, ItemDto.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+
+        return responseEntity.getBody();
     }
 
     public List<ItemDto> getCallList() {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/get-call-list")
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+        log.info("Body = " + responseEntity.getBody());
+
+        return fromJSONtoItems(responseEntity.getBody());
     }
 
     public ItemDto postCall(String query) {
@@ -24,5 +71,32 @@ public class RestTemplateService {
 
     public List<ItemDto> exchangeCall(String token) {
         return null;
+    }
+
+    // 위에서 리스트로 받아오면 이런 형태로 가져옴.
+//    {
+//        "items":[
+//        {"title":"Mac","price":3888000},
+//        {"title":"iPad","price":1230000},
+//        {"title":"iPhone","price":1550000},
+//        {"title":"Watch","price":450000},
+//        {"title":"AirPods","price":350000}
+//	]
+//    }
+
+
+
+    public List<ItemDto> fromJSONtoItems(String responseEntity) {
+        JSONObject jsonObject = new JSONObject(responseEntity);
+        // items 라는 key값으로 가져와서 각각의 json을 객체로 변환.
+        JSONArray items  = jsonObject.getJSONArray("items");
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (Object item : items) {
+            ItemDto itemDto = new ItemDto((JSONObject) item);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
     }
 }
