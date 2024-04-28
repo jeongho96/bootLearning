5de4aee07505752f46ebcb1219a4ef60b6221187
jeongho <jeongholee0918@gmail.com>
2024-04-28 19:23:35 +0900

    repository SQL문 설명 추가

--- MySelectShop_main/src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ MySelectShop_main/src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -11,7 +11,19 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByUser(User user, Pageable pageable);
 
-    // select * from
+//    select
+//        p.id,
+//        p.title as product_title,
+//        pf.product_id as product_id,
+//        pf.folder_id as folder_id
+//    from
+//            product p left join product_folder pf
+//                on p.id = pf.product_id
+//    where p.user_id = 1
+//        and
+//        pf.folder_id = 3
+//    order by p.id
+//        limit 12, 10; // 12번 부터 10개씩(이건 참조)
     // 사용자가 가진 상품 중에
     // product에 양방향으로 잡힌 productFolderList의 Folder의 folder_id를 기준으로 가져온다.
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
